5e09112d6526ed43f6a1a3c9e57ad9d35e2ea9f3
iot-Noob <talhakhalid2018@gmail.com>
2025-05-10 13:15:13 +0500

    added kwargs in mail sender to get param from users for mail to send

--- email_sender.py
+++ email_sender.py
@@ -10,24 +10,26 @@ load_dotenv(interpolate=True, override=True)
 
 
 class MailSender:
-    def __init__(self):
-           self.email = os.getenv("email")
-           self.password = os.getenv("password")
-           self.smtp_server = os.getenv("smtp_server")
-           self.port = os.getenv("port")
-           self.get_email_config()
- 
+    
+    def __init__(self, **kwargs):
+        self.email = kwargs.get("email") or os.getenv("email")
+        self.password = kwargs.get("password") or os.getenv("password")
+        self.smtp_server = kwargs.get("smtp_server") or os.getenv("smtp_server")
+        self.port = kwargs.get("port") or os.getenv("port")
+        
+        self.get_email_config()
+        
     def get_email_config(self):
         try:
 
             if not self.email:
-                raise ValueError("❌ email is missing in .env")
+                raise ValueError("❌ email is missing in .env or as param in class")
             if not self.password:
-                raise ValueError("❌ password is missing in .env")
+                raise ValueError("❌ password is missing in .env or as param in class")
             if not self.smtp_server:
-                raise ValueError("❌ smtp_server is missing in .env")
+                raise ValueError("❌ smtp_server is missing in .env or as param in class")
             if not self.port:
-                raise ValueError("❌ port is missing in .env")
+                raise ValueError("❌ port is missing in .env or as param in class")
 
             return self.email, self.password, self.smtp_server, int(self.port)
 
